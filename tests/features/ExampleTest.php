69ddfcb83efb5484e6af0971e782aa8633a5b596
Ruben <rubenperalta@hotmail.com>
2017-02-19 21:50:48 -0400

    Personalizar el Directorio Test

---
+++ tests/features/ExampleTest.php
@@ -0,0 +1,19 @@
+<?php
+
+class ExampleTest extends FeatureTestCase
+{
+    
+    function test_basic_example()
+    {
+        
+        $user = factory(\App\User::class)->create([
+            'name'=> 'Ruben Peralta',
+            'email'=> 'rubenperalta@hotmail.com',
+        ]);
+
+        $this->actiongAs($user, 'api')
+             ->visit(api/user)
+             ->see('Ruben Peralta')
+             ->see('rubenperalta@hotmail.com');
+    }
+}
